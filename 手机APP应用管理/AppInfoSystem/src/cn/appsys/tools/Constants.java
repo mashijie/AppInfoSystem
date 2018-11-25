e12e5170109629c3d5c033d5cc57e6984836407a
msj <1658037347@qq.com>
2018-11-25 13:14:17 +0800

    手机管理系统1.2

--- 手机APP应用管理/AppInfoSystem/src/cn/appsys/tools/Constants.java
+++ 手机APP应用管理/AppInfoSystem/src/cn/appsys/tools/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	public final static String SYS_MESSAGE = "message";
 	public final static int pageSize = 5;
 	public final static String FILEUPLOAD_ERROR_1 = " * APK信息不完整！";
-	public final static String FILEUPLOAD_ERROR_2 = " * 上传失败！";
+	public final static String FILEUPLOAD_ERROR_2 = " * 上传失败！！";
 	public final static String FILEUPLOAD_ERROR_3 = " * 上传文件格式不正确！";
 	public final static String FILEUPLOAD_ERROR_4 = " * 上传文件过大！";
 }
